e09ae6b98482214e431fd11a0b0706f5324dcdff
Artmaster629 <ponedilokartem2@gmail.com>
2025-09-19 13:20:44 +0300

    Миксин почищен от лишних импортов

--- src/main/java/net/artmaster/openpacbp/mixin/ConfigSetCommandMixin.java
+++ src/main/java/net/artmaster/openpacbp/mixin/ConfigSetCommandMixin.java
@@ -1,9 +1,6 @@
 package net.artmaster.openpacbp.mixin;
 
-import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.arguments.StringArgumentType;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.Commands;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
