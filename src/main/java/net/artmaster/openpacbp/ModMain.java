e81367758b86c19e88ffb2395cb4ca2bb1bd4999
Artmaster629 <ponedilokartem2@gmail.com>
2025-09-19 13:26:01 +0300

    Удалены лишние импорты и строки

--- src/main/java/net/artmaster/openpacbp/ModMain.java
+++ src/main/java/net/artmaster/openpacbp/ModMain.java
@@ -3,9 +3,8 @@ package net.artmaster.openpacbp;
 
 import com.mojang.logging.LogUtils;
 
-import net.artmaster.openpacbp.api.quests.MyAttachments;
+import net.artmaster.openpacbp.api.trades.MyAttachments;
 import net.artmaster.openpacbp.init.ModMenus;
-import net.artmaster.openpacbp.network.Network;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -13,7 +12,6 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
@@ -24,7 +22,6 @@ public class ModMain {
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    private static final PayloadRegistrar REGISTRAR = new PayloadRegistrar("openpacbp");
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
@@ -42,7 +39,6 @@ public class ModMain {
 
     private void commonSetup(FMLCommonSetupEvent event) {
         // Some common setup code
-
         LOGGER.info("Готов служить Мастерии!");
     }
 
