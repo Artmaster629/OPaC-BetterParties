84c2397587ada3a3afe99f2bb6373b861ff225e9
Artmaster629 <ponedilokartem2@gmail.com>
2025-09-19 13:25:24 +0300

    Добавлена корректная синхронизация, добавлены кнопки для покупки предмета, получение имени и цвета больше не требует пакета

--- src/main/java/net/artmaster/openpacbp/client/GlobalTradesScreen.java
+++ src/main/java/net/artmaster/openpacbp/client/GlobalTradesScreen.java
@@ -2,7 +2,7 @@ package net.artmaster.openpacbp.client;
 
 import net.artmaster.openpacbp.gui.GlobalTradesMenu;
 import net.artmaster.openpacbp.network.Network;
-import net.artmaster.openpacbp.network.SyncPartiesPacket;
+import net.artmaster.openpacbp.network.parties.SyncPartiesPacket;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -16,27 +16,44 @@ import java.util.List;
 
 public class GlobalTradesScreen extends AbstractContainerScreen<GlobalTradesMenu> {
     private static final ResourceLocation TEXTURE =
-            ResourceLocation.fromNamespaceAndPath("openpacbp", "textures/gui/quest_menu.png");
+            ResourceLocation.fromNamespaceAndPath("openpacbp", "textures/gui/market_storage_menu.png");
 
     public GlobalTradesScreen(GlobalTradesMenu menu, Inventory inv, Component title) {
         super(menu, inv, title);
         this.imageWidth = 176;
         this.imageHeight = 200;
+
     }
 
     public void setParties(List<SyncPartiesPacket.PartyData> parties) {
-        this.menu.getAllParties().updateParties(parties); // Добавим метод в GlobalTradesMenu/AllPartiesContainer
-        this.menu.reloadPage(); // Обновляем текущую страницу
+        this.menu.getAllParties().updateParties(parties);
+        this.menu.setParties(parties);
+        this.menu.reloadPage();
     }
+
 
     @Override
     protected void init() {
         super.init();
+
+
+        if (menu.getAllParties().getCurrentParty() != null) {
+            menu.getPartyName();
+            menu.getPartyColor();
+        }
+
         Network.requestPartyName(menu.getCurrentPage());
         Network.requestPartyColor(menu.getCurrentPage());
 
+
+
+
         int left = this.leftPos;
         int top = this.topPos;
+
+        int x = leftPos + 8;
+        int y = topPos + 20;
+
 
 
         // Кнопки
@@ -47,21 +64,65 @@ public class GlobalTradesScreen extends AbstractContainerScreen<GlobalTradesMenu
         this.addRenderableWidget(Button.builder(Component.literal(">"), b -> {
             if (menu.getCurrentPage() != menu.getTotalParties() -1) menu.nextPage();
         }).bounds(left + 27, top + 80, 20, 20).build());
-        this.addRenderableWidget(Button.builder(Component.literal("Гильдия"), b -> {
-            menu.partyPage();
-        }).bounds(left + 47, top + 80, 49, 20).build());
+
+
+        //Торговые кнопки
+
+        this.addRenderableWidget(Button.builder(Component.literal("✓"), b -> {
+
+            if (menu.getAllParties().getCurrentParty() == null) return;
+            Network.buyItem(menu.getCurrentPage(), 0, 1);
+        }).bounds(left + 96, top + 19, 18, 18).build());
+
+        this.addRenderableWidget(Button.builder(Component.literal("✓"), b -> {
+
+            if (menu.getAllParties().getCurrentParty() == null) return;
+            Network.buyItem(menu.getCurrentPage(), 2, 3);
+        }).bounds(left + 96, top + 40, 18, 18).build());
+
+        this.addRenderableWidget(Button.builder(Component.literal("✓"), b -> {
+
+            if (menu.getAllParties().getCurrentParty() == null) return;
+            Network.buyItem(menu.getCurrentPage(), 4, 5);
+        }).bounds(left + 96, top + 61, 18, 18).build());
+
+        this.addRenderableWidget(Button.builder(Component.literal("✓"), b -> {
+
+            if (menu.getAllParties().getCurrentParty() == null) return;
+            Network.buyItem(menu.getCurrentPage(), 6, 7);
+        }).bounds(left + 96, top + 82, 18, 18).build());
+
+
+
+        this.addRenderableWidget(Button.builder(Component.literal("Наша страница"), b -> {
+            Network.sendButtonClick("openpacbp market settings");
+        }).bounds(x, y, 70, 20).build());
     }
 
     @Override
     protected void renderLabels(GuiGraphics graphics, int mouseX, int mouseY) {
 
 
-        Component colorizedPartyName = Component.literal(Network.getPartyName())
-                .withStyle(style -> style.withColor(TextColor.fromRgb(Network.getPartyColor())));
+        String partyName = "Нет данных";
+        int partyColor;
+
+        if ((menu.getTotalParties() == 0)) return;
+        if ((menu.getAllParties() == null)) return;
+
+
+        if (menu.getAllParties().getCurrentParty() != null) {
+            partyName = menu.getPartyName();
+            partyColor = menu.getPartyColor();
+        } else {
+            partyColor = 0xAAAAAA;
+        }
+
+        Component colorizedPartyName = Component.literal(partyName)
+                .withStyle(style -> style.withColor(TextColor.fromRgb(partyColor)));
 
         graphics.drawString(this.font, this.title, this.titleLabelX, this.titleLabelY, 4210752, false);
         graphics.drawString(this.font, this.playerInventoryTitle, 8, this.imageHeight - 94, 4210752, false);
-        graphics.drawString(this.font, colorizedPartyName, 8, this.titleLabelY+20, 0xFFFFFF, true);
+        graphics.drawString(this.font, colorizedPartyName, 8, this.titleLabelY+40, 0xFFFFFF, true);
         graphics.drawString(this.font,
                 "Страница " + (menu.getCurrentPage() + 1) + "/" + menu.getTotalParties(),
                 this.titleLabelX+90, this.titleLabelY, 4210752, false);
